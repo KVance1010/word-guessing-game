686704a9a9780ccb640c16eb4a34d043bdaa6c43
MarcoGentil <113885531+MarcoGentil@users.noreply.github.com>
2022-09-30 11:22:59 -1000

    finish pseudocode

---
+++ index.js
@@ -0,0 +1,13 @@
+//Step 1:Create an array of string that the user can be able to 
+
+//2: create a variable for the timer, and one for the dwnbttn
+//3: create a start button for the usser, to start the timer 
+//3: creeate a random number generator that will be use to select the word from array
+//4: slice metod to brak the word in an array
+//5: use a .length to get the number of characters in the word
+//7: get the user input loop over the array of letter looking for matches
+//8: if there is a math display that letters
+//9: display incorrect letter 
+//10: if the usser gess the word or the count down its 0 display you win or you lose
+//11: add wins or losses games of the usser  
+//12: diplay wins and losses
